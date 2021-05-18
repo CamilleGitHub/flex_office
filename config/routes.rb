150b4e135186d180da74f1b0726193170cf53193
Camille Morin <dewavrin.camille@gmail.com>
2021-05-18 18:11:30 +0200

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   end
   resources :bookings, only: [:edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'dashboard', to: 'pages#dashboard'
 end
